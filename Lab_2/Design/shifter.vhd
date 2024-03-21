30c62e372cc5a240b7e5514d9a4e91c402ceeba9
giorgiozoccatelli <99725040+giorgiozoccatelli@users.noreply.github.com>
2024-03-21 09:11:40 +0100

    Update shifter.vhd

--- Lab_2/Design/shifter.vhd
+++ Lab_2/Design/shifter.vhd
@@ -12,27 +12,27 @@ ARCHITECTURE Behavior OF shifter IS
 	process(input, SW)
 		begin
 		if input = "000001010010011" then -- caso HELLO
-			if SW(2 downto 0) = "000" then
+			if SW(4 downto 2) = "000" then
 				output <= "000001010010011"; -- caso HELLO
-			elsif SW(2 downto 0) = "001" then
+			elsif SW(4 downto 2) = "001" then
 				output <= "001010010011000"; -- caso ELLOH
-			elsif SW(2 downto 0) = "010" then
+			elsif SW(4 downto 2) = "010" then
 				output <= "010010011000001"; -- caso LLOHE
-			elsif SW(2 downto 0) = "011" then
+			elsif SW(4 downto 2) = "011" then
 				output <= "010011000001010"; -- caso LOHEL
-			elsif SW(2 downto 0) = "110" then
+			elsif SW(4 downto 2) = "110" then
 				output <= "011000001010010"; -- caso OHELL
 			end if
 		elsif input = "100001101101011" then --caso CEPPO
-			if SW(2 downto 0) = "000" then
+			if SW(4 downto 2) = "000" then
 				output <= "100001101101011"; -- caso CEPPO
-			elsif SW(2 downto 0) = "001" then
+			elsif SW(4 downto 2) = "001" then
 				output <= "001101101011100"; -- caso EPPOC
-			elsif SW(2 downto 0) = "010" then
+			elsif SW(4 downto 2) = "010" then
 				output <= "101101011100001"; -- caso PPOCE
-			elsif SW(2 downto 0) = "011" then
+			elsif SW(4 downto 2) = "011" then
 				output <= "101011100001101"; -- caso POCEP
-			elsif SW(2 downto 0) = "110" then
+			elsif SW(4 downto 2) = "110" then
 				output <= "011100001101101"; -- caso OCEPP
 			end if
 			
