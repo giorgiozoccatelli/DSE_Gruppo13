7a2e6040401f9a233f40a66a018f68a50572538e
giorgiozoccatelli <99725040+giorgiozoccatelli@users.noreply.github.com>
2024-03-22 13:20:20 +0100

    Update mux_3_5to1.vhd

--- Lab_2/Design/mux_3_5to1.vhd
+++ Lab_2/Design/mux_3_5to1.vhd
@@ -1,25 +1,24 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY mux_3_5to1 IS
+ENTITY mux IS
 	PORT ( SW: IN STD_LOGIC_VECTOR(1 downto 0);
-			output : OUT STD_LOGIC_VECTOR(14 downto 0));
-END mux_3_5to1;
+			output_mux : OUT STD_LOGIC_VECTOR(14 downto 0));
+END mux;
 
-ARCHITECTURE Behavior OF mux_3_5to1 IS
+ARCHITECTURE Behavior OF mux IS
 	begin
 		process(SW)
 		begin
 			if SW(1 downto 0) = "00" then
-				output <= "000001010010011"; -- HELLO
+				output_mux <= "000001010010011"; -- HELLO
 			elsif SW(1 downto 0) = "01" then
-				output <= "100001101101011"; -- CEPPO
+				output_mux <= "100001101101011"; -- CEPPO
 			elsif SW(1 downto 0) = "10" then
-				output <= "100001010010011"; -- CELLO
+				output_mux <= "100001010010011"; -- CELLO
 			elsif SW(1 downto 0) = "11" then
-				output <= "110001101101011"; -- FEPPO
+				output_mux <= "110001101101011"; -- FEPPO
 			end if;
 	end process;
 END Behavior;
-
 
