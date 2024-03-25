0165bed7a4a93a327fbec8062aacf63fb8a6a09b
giorgiozoccatelli <99725040+giorgiozoccatelli@users.noreply.github.com>
2024-03-25 11:00:57 +0100

    Update converter_display_tb.vhd

--- Lab_2/Testbench/converter_display_tb.vhd
+++ Lab_2/Testbench/converter_display_tb.vhd
@@ -11,11 +11,11 @@ end entity converter_display_tb;
 architecture behavior of converter_display_tb is
 	component converter_display
 	PORT ( SW : IN unsigned(3 DOWNTO 0);
-			 HEX0,HEX1 : OUT STD_LOGIC_VECTOR(0 to 6)
+			 HEX1,HEX0 : OUT STD_LOGIC_VECTOR(0 to 6)
 			);
 	end component;
 	
-signal SW_tb: unsigned(4 downto 0);
+signal SW_tb: unsigned(3 downto 0);
 
 begin	
 		dut: converter_display port map(
