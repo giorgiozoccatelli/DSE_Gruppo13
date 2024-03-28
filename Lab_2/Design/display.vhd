5f3d6d18abdf21d34fdb6f5e5d117d30f9c33d5b
giorgiozoccatelli <99725040+giorgiozoccatelli@users.noreply.github.com>
2024-03-28 15:45:51 +0100

    Update display.vhd

--- Lab_2/Design/display.vhd
+++ Lab_2/Design/display.vhd
@@ -1,62 +1,51 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity display is
-    Port ( m : in std_logic_vector(3 downto 0);  
-           z : in std_logic;
-			  HEX1, HEX0: out std_logic_vector(0 to 6)
-    );
+	port(m: in std_logic_vector(3 downto 0);
+			z: std_logic;
+		  HEX1, HEX0 : out std_logic_vector(0 to 6)
+		  );
 end display;
 
-architecture behavior of display is
-begin
-	process(m, z)
-	begin
-		if z='0' then
-			HEX1 <= "1111111";
-			case m is
-            when "0000" =>
-                HEX0 <= "0000001"; -- 0 in formato 7 segmenti
-            when "0001" =>
-                HEX0 <= "1001111"; -- 1 in formato 7 segmenti
-            when "0010" =>
-                HEX0 <= "0010010"; -- 2 in formato 7 segmenti
-            when "0011" =>
-                HEX0 <= "0000110"; -- 3 in formato 7 segmenti
-            when "0100" =>
-                HEX0 <= "1001100"; -- 4 in formato 7 segmenti
-            when "0101" =>
-                HEX0 <= "0100100"; -- 5 in formato 7 segmenti
-            when "0110" =>
-                HEX0 <= "0100000"; -- 6 in formato 7 segmenti
-            when "0111" =>
-                HEX0 <= "0001111"; -- 7 in formato 7 segmenti
-            when "1000" =>
-                HEX0 <= "0000000"; -- 8 in formato 7 segmenti
-            when "1001" =>
-                HEX0 <= "0000100"; -- 9 in formato 7 segmenti
-            when others =>
-                HEX0 <= "1111111"; -- In caso di valore non valido, disattiva tutti i segmenti
-			end case;
-		else 
-			HEX1 <= "1001111";
-			case m is 
-					when "0000" =>
-                HEX0 <= "0000001"; -- 0 in formato 7 segmenti
-            when "0001" =>
-                HEX0 <= "1001111"; -- 1 in formato 7 segmenti
-            when "0010" =>
-                HEX0 <= "0010010"; -- 2 in formato 7 segmenti
-            when "0011" =>
-                HEX0 <= "0000110"; -- 3 in formato 7 segmenti
-            when "0100" =>
-                HEX0 <= "1001100"; -- 4 in formato 7 segmenti
-            when "0101" =>
-                HEX0 <= "0100100"; -- 5 in formato 7 segmenti
-	    when others =>
-		HEX0 <= "1111111";  -- In caso di valore non valido, disattiva tutti i segmenti
-			end case;
-		end if;
+architecture behavioral of display is
+	begin 
+	process(z,m)
+		begin
+		
+		case z is
+			when '0' => --0
+				HEX1 <= "1111111";
+			when '1' => --1
+				HEX1 <= "1001111";
+			when others =>
+				HEX1 <= "1111111";
+		end case;
+		
+		case m is 
+			when "0000" => --0
+				HEX0 <= "1111111";
+			when "0001" => --1
+				HEX0 <= "1001111";	
+			when "0010" => --2
+				HEX0 <= "0010010";
+			when "0011" => --3
+				HEX0 <= "0000110";
+			when "0100" => --4
+				HEX0 <= "1001100";
+			when "0101" => --5
+				HEX0 <= "0100100";
+			when "0110" => --6
+				HEX0 <= "0100000";
+			when "0111" => --7
+				HEX0 <= "0001111";
+			when "1000" => --8
+				HEX0 <= "0000000";
+			when "1001" => --9
+				HEX0 <= "0000100";
+			when others =>
+				HEX0 <= "1111111";
+		end case;
+		
 	end process;
-end behavior;
+end behavioral;
